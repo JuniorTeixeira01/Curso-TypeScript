23665254763a8e3f4514ea90c3f242582e51cc22
Junior Teixeira <teixeirajunior19051988@gmail.com>
2024-08-05 22:28:31 -0300

    Criação de arquivos e acrécimo de conteúdo

---
+++ FUNDAMENTOS/index.js
@@ -0,0 +1,7 @@
+var x = 10;
+console.log(x);
+console.log(typeof x);
+var y = 15.487658;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
---
+++ FUNDAMENTOS/index.ts
@@ -0,0 +1,10 @@
+let x: number = 10;
+console.log(x);
+
+console.log(typeof x)
+
+const y = 15.487658;
+
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
